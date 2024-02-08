fef861e09c01f075e36d70d75ff99900418e3da8
tsolina <tsolina@gmail.com>
2024-02-09 00:42:47 +0100

    another example added

---
+++ examples/hide_selected_item..py
@@ -0,0 +1,8 @@
+# basic example to hide selected item
+# part must be open to run this example
+from experience import *
+
+# accessing application
+app = experience_application()
+
+app.active_editor().selection().vis_properties().set_show(1)
